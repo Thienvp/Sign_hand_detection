6b9c82089a400cba241030234d297b9b5c21f479
KUMA <tienquanphan2003@gmail.com>
2023-07-10 11:01:50 +0700

    add data

--- create_dataset.py
+++ create_dataset.py
@@ -1,12 +1,16 @@
 import numpy as np
 import os
-# Trước khi chạy kéo file A.npy ra khỏi folder "data" và cùng cấp với nó
-path_1 = "./A.npy"
-data = np.load(path_1)
 path_dir = "./data"
+temp = False
+data = None
 for file in os.listdir(path_dir):
-    A = np.load(path_dir+"/"+file)
-    data = np.concatenate((data, A), axis = 0)
+    print(file)
+    _data = np.load(path_dir+"/"+file)
+    if(temp):
+        data = np.concatenate((data, _data), axis = 0)
+    else:
+        data = _data
+        temp = True
 
 print(data.shape)
 np.save("./dataset/data.npy", data)
